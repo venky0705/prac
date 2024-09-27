d230fc4a3366050542d0dadcd96664822555ee5c
Vemala <venkatesh.vemala@siemens-healthineers.com>
2024-09-27 19:44:41 +0200

    chages for calculator.py programming for branch copy2

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a =int(input("Enter a number: "))
+b = int(input("Enter b: "))
+
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
+addition(a,b)
+subtraction(a,b)
+multiplication(a,b)
+division(a,b)
+print(addition(a,b))
+print(subtraction(a,b))
+print(multiplication(a,b))
+print(division(a,b))
